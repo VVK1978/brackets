8079ff74a21204dc62208bfee5c44c28a2830e9a
VVK1978 <70840645+VVK1978@users.noreply.github.com>
2020-10-25 21:39:20 +0300

    solution brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	const stack = [];
+	const brackets = Object.fromEntries(bracketsConfig);
+
+	for (const element of str.split``) {   //массив из строки str
+		 if (stack[0] === element) {
+			  stack.shift();            // удаление 1 эл массива
+		 } else if (brackets[element]) {
+			  stack.unshift(brackets[element]);  // добавление эл массива
+		 } else {
+			  return false;
+		 }
+	}
+
+	return stack.length === 0; // true если длина массива пуста
 }
+
